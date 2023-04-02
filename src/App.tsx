bc679fc8be2907605b1f7c668f42aeee1257f33d
Daniel Merillas <dmerillasg@minsait.com>
2023-04-02 02:27:34 +0200

    Cargando de pacman

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import './App.css';
 import { NewButtons } from './components/NewButtons';
 import { ReactTable } from './components/ReactTable';
 import { borrarDato, obtenerDatos } from './services/db';
+import { PacmanLoader } from 'react-spinners';
 
 function App() {
 
   const [data, setData] = useState<any>([])
+  const [loading, setLoading] = useState<boolean>(true)
 
   const obtenerDatosTabla = () => {
+    setLoading(true)
     obtenerDatos("PreciosSuperMercados", "PRODUCT_NAME,SUPERMERCADO,PRECIO,PRECIO_UNIDAD,CANTIDAD,id", "PRODUCT_NAME").then((results) => {
       const values = results ? results.data : [];
       setData(values)
+      setLoading(false)
     })
   }
 
@@ -41,8 +45,8 @@ function App() {
       <header className="App-header">
         <NewButtons updateTableEvent={obtenerDatosTabla} />
       </header>
-      <section>
-        <ReactTable data={data} eventoBorrar={borrarDatos} />
+      <section className={loading ? 'loadingSection' : ''}>
+        {!loading ? <ReactTable data={data} eventoBorrar={borrarDatos} /> : <PacmanLoader color="#36d7b7" />}
       </section>
     </div>
   );
